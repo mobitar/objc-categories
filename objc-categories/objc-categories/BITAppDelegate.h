0bbc22a965253097b8b90bc8a6c61dfdab1f072e
Mo Bitar <me@mbbitar.com>
2013-04-06 19:51:59 -0500

    initial commit

---
+++ objc-categories/objc-categories/BITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BITAppDelegate.h
+//  objc-categories
+//
+//  Created by Mo Bitar on 4/6/13.
+//  Copyright (c) 2013 bitar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ objc-categories/objc-categories/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  objc-categories
+//
+//  Created by Mo Bitar on 4/6/13.
+//  Copyright (c) 2013 bitar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BITAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BITAppDelegate class]));
+    }
+}
