e8a87225906edd5f6c2d285c4bf1dc8fe965cb50
Mo Bitar <me@mbbitar.com>
2013-04-09 13:33:46 -0500

    refactored macros into their own file

--- objc-categories/Categories.h
+++ objc-categories/Categories.h
@@ -7,16 +7,4 @@
 #import "UIScrollView+Utilities.h"
 #import "NSTimer+Blocks.h"
 #import "VTPG_Common.h"
-
-#define $str(...) [NSString stringWithFormat:__VA_ARGS__]
-
-#define $img(name) [UIImage imageNamed:name]
-
-#define $eql(obj1, obj2) [obj1 isEqual:obj2]
-
-#define return_if_equal(obj1, obj2) \
-    if($eql(obj1, obj2)) return
-
-#define $is(instance, cls) [instance isKindOfClass:[cls class]]
-
-#define $new(cls, varname) cls *varname = [cls new]
+#import "Macros.h"
---
+++ objc-categories/Macros.h
@@ -0,0 +1,50 @@
+/*
+ * creates a formatted string
+ * usage: $str(@"My number:%i", 5);
+ */
+#define $str(...) [NSString stringWithFormat:__VA_ARGS__]
+
+/*
+ * returns an image with given name
+ * usage: $img(@"myImage.png");
+ */
+#define $img(name) [UIImage imageNamed:name]
+
+/*
+ * checks if two objects are equal
+ * usage: if($eql(var1, var2))...
+ */
+#define $eql(obj1, obj2) [obj1 isEqual:obj2]
+
+/*
+ * returns if both objects are equal
+ * (used primarily in setter methods)
+ * usage: if($return_if_eql(var1, var2))...
+ */
+#define $return_if_equal(obj1, obj2) \
+    if($eql(obj1, obj2)) return
+
+/*
+ * checks if the instance is of type cls
+ * usage: if($is(val, UIFont))...
+ */
+#define $is(instance, cls) [instance isKindOfClass:[cls class]]
+
+/*
+ * creates a local variable named varname of type cls
+ * usage: $new(NSArray, myArray)
+ * int count = myArray.count;
+ */
+#define $new(cls, varname) cls *varname = [cls new]
+
+/*
+ * creates a local "weakself" variable
+ * usage: @createWeakSelf
+ */
+#define createWeakSelf try {} @finally {} __weak typeof(self) weakself = self;
+
+/*
+ * replaces occurences of find value in string with replace value
+ * usage: $stringreplace(myString, @"cat", @"dog") 
+ */
+#define $stringreplace(string, find, replace) [string stringByReplacingOccurrencesOfString:find withString:replace];
