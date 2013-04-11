e148ebc1cdabb037f156da40d0e9f15beb7920d4
Mo Bitar <me@mbbitar.com>
2013-04-11 18:47:30 -0500

    added functions

--- objc-categories/Functions.h
+++ objc-categories/Functions.h
@@ -1,1 +1,3 @@
 void applyBlockToObjects(NSArray *objects, void(^block)(id object));
+void applyBlockToObjectsWithDelayBetweenObjects(NSArray *objects, CGFloat delayBetweenObjects, void(^block)(id object));
+void applyBlockToObjectsWithDelayBetweenObjectsAndCompletion(NSArray *objects, CGFloat delayBetweenObjects, void(^block)(id object), void(^completion)());
--- objc-categories/Functions.m
+++ objc-categories/Functions.m
@@ -3,3 +3,23 @@ void applyBlockToObjects(NSArray *objects, void(^block)(id object)) {
         block(obj);
     }
 }
+
+void applyBlockToObjectsWithDelayBetweenObjects(NSArray *objects, CGFloat delayBetweenObjects, void(^block)(id object)) {
+    CGFloat currentOffset = 0;
+    for(id obj in objects) {
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(currentOffset * NSEC_PER_SEC));
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            block(obj);
+        });
+        currentOffset += delayBetweenObjects;
+    }
+}
+
+void applyBlockToObjectsWithDelayBetweenObjectsAndCompletion(NSArray *objects, CGFloat delayBetweenObjects, void(^block)(id object), void(^completion)()) {
+    CGFloat totalOffset = ((objects.count - 1) * delayBetweenObjects) + delayBetweenObjects;
+    applyBlockToObjectsWithDelayBetweenObjects(objects, delayBetweenObjects, block);
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(totalOffset * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        completion();
+    });
+}
