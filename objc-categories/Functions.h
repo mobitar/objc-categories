5272e7133bd6ef9e43bcd779741d62c799eef059
Mo Bitar <me@mbbitar.com>
2013-04-11 18:48:42 -0500

    functions

--- objc-categories/Functions.h
+++ objc-categories/Functions.h
@@ -1,3 +1,4 @@
+//Functions.h
 void applyBlockToObjects(NSArray *objects, void(^block)(id object));
 void applyBlockToObjectsWithDelayBetweenObjects(NSArray *objects, CGFloat delayBetweenObjects, void(^block)(id object));
 void applyBlockToObjectsWithDelayBetweenObjectsAndCompletion(NSArray *objects, CGFloat delayBetweenObjects, void(^block)(id object), void(^completion)());
